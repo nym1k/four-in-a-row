f734f6915744f41c3480c3fe906eb9113e538240
Ross King <sleeping.stars@gmail.com>
2019-06-06 08:03:14 +0100

    Course beginning up to rendering the game

---
+++ js/Token.js
@@ -0,0 +1,22 @@
+class Token {
+  constructor(index, owner) {
+    this.owner = owner;
+    this.id = `token-${index}-${owner.id}`;
+    this.dropped = false;
+  }
+
+  /**
+   * Draws new HTML token.
+   */
+  drawHTMLToken() {
+    const token = document.createElement('div');
+    document.getElementById('game-board-underlay').appendChild(token);
+    token.setAttribute('id', this.id);
+    token.setAttribute('class', 'token');
+    token.style.backgroundColor = this.owner.color;
+  }
+
+  get htmlToken() {
+    this.drawHTMLToken()
+  }
+}
